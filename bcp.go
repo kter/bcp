9b88f82a579349218b99b2d66ed3410f434be177
kter <me@kter.jp>
2018-02-03 06:00:28 +0000

    ability to create old dir

--- bcp.go
+++ bcp.go
@@ -9,7 +9,7 @@ import (
 	"gopkg.in/urfave/cli.v1"
 )
 
-func exists(path string) (bool, error) {
+func dir_existence(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err == nil {
 		return true, nil
@@ -26,20 +26,27 @@ func main() {
 	app.Usage = "bcp backup-file-name"
 	app.Action = func(c *cli.Context) error {
 		sourceFile := c.Args().Get(0)
-		fmt.Printf("file %q", sourceFile)
 
 		dir, err := os.Getwd()
 		if err != nil {
 			cli.NewExitError("Unable to get current location", 1)
 		}
-		fmt.Printf(dir)
-		// olddir := dir + "/old"
+		fmt.Printf("sourceFile: %s\n", sourceFile) // debug
+		fmt.Printf("dir: %s\n", dir)               // debug
+		olddir := dir + "/old"
+		fmt.Printf("olddir: %s\n", olddir) // debug
 
-		// if dir != 0 {
-		// 	fmt.Printf("determine prefix number")
-		// } else {
-		// 	fmt.Printf("create new old dir")
-		// }
+		dirExists, err := dir_existence(olddir)
+		if err != nil {
+			cli.NewExitError("Unable to get destination dir location", 1)
+		}
+		fmt.Printf("dirExists: %t\n", dirExists)
+		if dirExists == true {
+			fmt.Printf("determine prefix number")
+		} else {
+			fmt.Printf("create new old dir")
+			os.Mkdir(olddir, 0755)
+		}
 
 		// prefix := "-01"
 		// if prefix == nil {
