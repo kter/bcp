83a6c3ec57ca3fb9fcb9de3d98b398c6f607f091
kter <me@kter.jp>
2018-02-03 14:01:28 +0000

    delete debug code

--- bcp.go
+++ bcp.go
@@ -31,39 +31,30 @@ func main() {
 		if err != nil {
 			cli.NewExitError("Unable to get current location", 1)
 		}
-		fmt.Printf("sourceFile: %s\n", sourceFile) // debug
-		fmt.Printf("dir: %s\n", dir)               // debug
 		olddir := dir + "/old"
-		fmt.Printf("olddir: %s\n", olddir) // debug
 
 		dirExists, err := file_existence(olddir)
 		if err != nil {
 			cli.NewExitError("Unable to get destination dir location", 1)
 		}
-		fmt.Printf("dirExists: %t\n", dirExists) //debug
 		now := time.Now()
 		date := now.Format("20060102")
 		destinationFile := ""
 		if dirExists == true {
-			fmt.Printf("determine prefix number\n")
 			for prefix := 1; prefix < 100; prefix++ {
 				destinationFile = olddir + "/" + sourceFile + "." + date + "-" + fmt.Sprintf("%02d", prefix)
-				fmt.Printf("destinationFile: %s\n", destinationFile)
 				fileExists, err := file_existence(destinationFile)
 				if err != nil {
 					cli.NewExitError("Unable to get destination file location", 1)
 				}
-				fmt.Printf("fileExists: %t\n", fileExists)
 				if fileExists == false {
 					break
 				}
 			}
 		} else {
-			fmt.Printf("create new old dir\n")
 			os.Mkdir(olddir, 0755)
 			destinationFile = olddir + "/" + sourceFile + "." + date + "-01"
 		}
-		fmt.Printf("destinationFile: %s", destinationFile)
 
 		b, err := ioutil.ReadFile(sourceFile)
 		if err != nil {
