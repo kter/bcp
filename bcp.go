3326627af0562ad0c3a3883b9d1bca35a2cddfea
kter <me@kter.jp>
2018-02-03 13:50:14 +0000

    取りあえず動くように

--- bcp.go
+++ bcp.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
-	// "io/ioutil"
-	// "time"
+	"time"
 
 	"gopkg.in/urfave/cli.v1"
 )
 
-func dir_existence(path string) (bool, error) {
+func file_existence(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err == nil {
 		return true, nil
@@ -36,39 +36,43 @@ func main() {
 		olddir := dir + "/old"
 		fmt.Printf("olddir: %s\n", olddir) // debug
 
-		dirExists, err := dir_existence(olddir)
+		dirExists, err := file_existence(olddir)
 		if err != nil {
 			cli.NewExitError("Unable to get destination dir location", 1)
 		}
-		fmt.Printf("dirExists: %t\n", dirExists)
+		fmt.Printf("dirExists: %t\n", dirExists) //debug
+		now := time.Now()
+		date := now.Format("20060102")
+		destinationFile := ""
 		if dirExists == true {
-			fmt.Printf("determine prefix number")
+			fmt.Printf("determine prefix number\n")
+			for prefix := 1; prefix < 100; prefix++ {
+				destinationFile = olddir + "/" + sourceFile + "." + date + "-" + fmt.Sprintf("%02d", prefix)
+				fmt.Printf("destinationFile: %s\n", destinationFile)
+				fileExists, err := file_existence(destinationFile)
+				if err != nil {
+					cli.NewExitError("Unable to get destination file location", 1)
+				}
+				fmt.Printf("fileExists: %t\n", fileExists)
+				if fileExists == false {
+					break
+				}
+			}
 		} else {
-			fmt.Printf("create new old dir")
+			fmt.Printf("create new old dir\n")
 			os.Mkdir(olddir, 0755)
+			destinationFile = olddir + "/" + sourceFile + "." + date + "-01"
 		}
+		fmt.Printf("destinationFile: %s", destinationFile)
 
-		// prefix := "-01"
-		// if prefix == nil {
-
-		// }
-
-		// now := time.Now()
-		// date :=  now.Format("20060102")
-
-		// srcFileName := c.Args()
-		// dstFileName := srcFileName +  +
-
-		// b, err := ioutil.ReadFile("input.txt")
-		// if err != nil {
-		//     panic(err)
-		// }
-
-		// // write the whole body at once
-		// err = ioutil.WriteFile("output.txt", b, 0644)
-		// if err != nil {
-		//     panic(err)
-		// }
+		b, err := ioutil.ReadFile(sourceFile)
+		if err != nil {
+			panic(err)
+		}
+		err = ioutil.WriteFile(destinationFile, b, 0644)
+		if err != nil {
+			panic(err)
+		}
 		return nil
 	}
 
